d6b773d361a664b4937926df49f76a4b1cea4b82
ryoga-work <kubota.ryoga.work@gmail.com>
2024-04-30 16:35:37 +0900

    テストのクリア

--- app.js
+++ app.js
@@ -6,13 +6,15 @@
  * @returns {Number}
  */
 function factorial(n) {
-  let result = 1;
-  // TODO このコメントを消して正しく実装してください。
+  let result =1
+  for(let i=1 ;i<=n;i++){
+    result = result*i
+  }
   return result;
 }
 const assert = require('node:assert');
-assert.strictEqual(factorial(1), 1, `1の階乗は1ですが、実際は${factorial(1) }でした`);
-assert.strictEqual(factorial(2), 2, `2の階乗は2ですが、実際は${factorial(2) }でした`);
-assert.strictEqual(factorial(3), 6, `3の階乗は6ですが、実際は${factorial(3) }でした`);
+assert.strictEqual(factorial(1), 1, `1の階乗は1ですが、実際は${factorial(1)}でした`);
+assert.strictEqual(factorial(2), 2, `2の階乗は2ですが、実際は${factorial(2)}でした`);
+assert.strictEqual(factorial(3), 6, `3の階乗は6ですが、実際は${factorial(3)}でした`);
 assert.strictEqual(factorial(10), 3628800, `10の階乗は3628800ですが、実際は${factorial(10) }でした`);
 console.log('すべてのテストを通過しました');
